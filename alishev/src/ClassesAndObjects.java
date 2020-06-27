845b4cdf680ba2f2a168b2f2d020d494bcc1f5bb
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-06-27 21:24:31 +0300

    Add files via upload

--- alishev/src/ClassesAndObjects.java
+++ alishev/src/ClassesAndObjects.java
@@ -4,18 +4,14 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
         Person person1 = new Person();
-        person1.name = "Роман";
-        person1.age = 50;
+        person1.setNameAndAge("Роман", 20);
+        String s1 = "Вова";
 
         Person person2 = new Person();
-        person2.name = "Вова";
-        person2.age = 20;
+        person2.setNameAndAge(s1, 30);
 
-        int year1 = person1.calculateYearsToRetirement();
-        int year2 = person2.calculateYearsToRetirement();
-
-        System.out.println("Первому человеку до пенсии: " + year1 + " лет");
-        System.out.println("Второму человеку до пенсии: " + year2 + " лет");
+        person1.speak();
+        person2.speak();
     }
 }
 
@@ -25,6 +21,11 @@ class Person {
     // 2. Действия, которые он может совершать (методы)
     String name;
     int age;
+
+    void setNameAndAge(String username, int userage) {
+        name = username;
+        age = userage;
+    }
 
     int calculateYearsToRetirement() {
         int years = 65 - age;
