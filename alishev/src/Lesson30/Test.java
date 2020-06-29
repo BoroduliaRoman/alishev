bacdd41d9b3d86a2feb6621f645966077d019683
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-06-29 16:47:44 +0300

    Add files via upload

---
+++ alishev/src/Lesson30/Test.java
@@ -0,0 +1,37 @@
+package Lesson30;
+
+/**
+ * Created by BR 29.06.2020
+ */
+public class Test {
+    public static void main(String[] args) {
+        //int x = 23;
+        //short x = 123;
+        //byte x = 123; //-128 - 127
+        //long y = 123;
+        //double a = 12.3;
+        //float q = 12.2f;
+
+        /*
+        float f = (float) 123.2;
+        int a = 123;
+        long l = a; //не явное
+        int x = (int)l; //явное
+        System.out.println(x);
+         */
+
+        //int a = 123;
+        /*
+        double x = 123.5;
+        int y = (int) x;
+
+        System.out.println(y);
+
+        long l = Math.round(x);
+        System.out.println(l);
+         */
+
+        byte x = (byte) 129;
+        System.out.println(x);
+    }
+}
