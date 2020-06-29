9762184bb853fa9ea70d6a6314cc054846d3269b
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-06-29 10:28:50 +0300

    Add files via upload

--- alishev/src/Lesson23.java
+++ alishev/src/Lesson23.java
@@ -15,9 +15,27 @@ public class Lesson23 {
         String stringAll = string1 + string2 + string3;
         System.out.println(stringAll);
          */
+        /*
         StringBuilder sb = new StringBuilder("Hello");
         System.out.println(sb.toString());
         sb.append(" my").append(" friend");
         System.out.println(sb.toString());
+         */
+        //System.out.printf("This is a string, %s", "NICE");
+        //System.out.printf("This is a string, %d", 5);
+        //System.out.printf("%f This is %s a string, %d", 10.4, "HIHI", 10000);
+        /*
+        System.out.printf("String %10d \n", 532);
+        System.out.printf("String %10d \n", 5);
+        System.out.printf("String %10d \n", 100000);
+        System.out.printf("String %10d \n", 1000000000);
+         */
+
+        System.out.printf("String %.2f \n", 45.32265);
+        System.out.printf("String %.2f \n", 45.32);
+        System.out.printf("String %.2f \n", 45.33754);
+        System.out.printf("String %.2f \n", 45.32265546);
+        System.out.printf("String %.2f \n", 45.337);
+        // % -[10][.42]
     }
 }
