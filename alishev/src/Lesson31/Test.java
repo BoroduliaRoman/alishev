fcea900fb953fa386764ae4709fb7553419ab6ed
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-06-29 17:19:17 +0300

    Add files via upload

---
+++ alishev/src/Lesson31/Test.java
@@ -0,0 +1,19 @@
+package Lesson31;
+
+/**
+ * Created by BR 29.06.2020
+ */
+public class Test {
+    public static void main(String[] args) {
+        int x = 3;
+        //System.out.println(x);
+
+        //Integer x2 = new Integer(123);
+        Integer x2 = 123;
+        int y = x2;
+
+        Integer.parseInt("123");
+
+        // Double, Float, Lang, Integer, Short, Byte, Character, Boolean
+    }
+}
